259b7bcaa8da99834cbc4656149979f81951e902
rohansinghwow <rohansinghwow@gmail.com>
2022-04-09 12:36:45 +0530

    styling input box

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -14,7 +14,7 @@ export default function SearchBox(){
     return (
         <div className='text-center'>
         <label for="cocktail" className="block mb-2 mt-5 text-sm font-medium text-gray-900 dark:text-gray-300">Search a Cocktail</label>
-        <input type="text" onChange={handleCocktailSearch} id="cocktail" ref={searchInput} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/5 mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Blue Margarita" />
+        <input type="text" onChange={handleCocktailSearch} id="cocktail" ref={searchInput} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block md:w-1/5 mx-auto p-2.5 dark:bg-gray-700 dark:border-gray-600  w-full dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Blue Margarita" />
         
         </div>
     )
